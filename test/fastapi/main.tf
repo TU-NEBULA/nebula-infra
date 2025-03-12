b51818303c07cc7402f7e3786cb08b0fe02a46fc
wonjun0120 <wonjundero@gmail.com>
2025-03-12 22:25:17 +0900

    fix: Terraform FastAPI 워크플로우에서 EBS 볼륨 크기 및 타입 수정

--- test/fastapi/main.tf
+++ test/fastapi/main.tf
@@ -8,10 +8,10 @@ data "aws_key_pair" "existing_key" {
 
 resource "aws_instance" "fastapi_servers" {
   count         = 3
-  ami           = "ami-024ea438ab0376a47"  
+  ami           = "ami-024ea438ab0376a47"
   instance_type = var.instance_type
-  availability_zone = "ap-northeast-2a"  
-  subnet_id     = module.vpc.public_subnet_id  
+  availability_zone = "ap-northeast-2a"
+  subnet_id     = module.vpc.public_subnet_id
   key_name      = data.aws_key_pair.existing_key.key_name
 
   vpc_security_group_ids = [
@@ -27,14 +27,14 @@ resource "aws_instance" "fastapi_servers" {
   }
 
   root_block_device {
-    volume_size = count.index == 2 ? 20 : count.index == 0 ? 10 : 8  # ✅ Chroma(20GB), FastAPI(10GB), Celery(8GB 기본값)
+    volume_size = count.index == 0 ? 20 : 8
+    volume_type = "gp3"
   }
 
   user_data = file("${path.module}/scripts/setup_docker.sh")
   user_data_replace_on_change = true
 }
 
-# ✅ FastAPI용 추가 EBS 볼륨 (10GB)
 resource "aws_ebs_volume" "fastapi_ebs" {
   availability_zone = "ap-northeast-2a"
   size             = 10
@@ -49,7 +49,7 @@ resource "aws_volume_attachment" "fastapi_attach" {
   instance_id = aws_instance.fastapi_servers[0].id
 }
 
-# ✅ Chroma용 추가 EBS 볼륨 (20GB)
+
 resource "aws_ebs_volume" "chroma_ebs" {
   availability_zone = "ap-northeast-2a"
   size             = 20
