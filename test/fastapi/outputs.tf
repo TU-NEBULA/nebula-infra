3179c6e79c2fa4cd8f34e343e522a250a59f0054
wonjun0120 <wonjundero@gmail.com>
2025-03-12 23:09:10 +0900

    refactor: EIP 출력 이름을 nebula_ai_eip로 변경하고 불필요한 출력 제거

--- test/fastapi/outputs.tf
+++ test/fastapi/outputs.tf
@@ -1,11 +1,3 @@
-output "fastapi_eip" {
-  value = aws_eip.fastapi_eip.public_ip
-}
-
-output "celery_eip" {
-  value = aws_eip.celery_eip.public_ip
-}
-
-output "chroma_eip" {
-  value = aws_eip.chroma_eip.public_ip
+output "nebula_ai_eip" {
+  value = aws_eip.nebula_ai_eip.public_ip
 }
